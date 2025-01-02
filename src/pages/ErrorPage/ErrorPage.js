6598db74decee6f8a1a5d1a55fdc2844e8bf2832
Roy Schor <rschor@justworks.com>
2025-01-01 22:36:12 -0500

    added new blog page, error page handling with appropriate routing, site logo rotation, another font, and other minor styling changes

---
+++ src/pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/layout.css";
+import "./ErrorPage.css";
+import errorGif from "../../assets/error-page-gif-no-bg.gif";
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="page-container error-page">
+      <div className="container">
+        <div className="page-content">
+          <Link to="/" className="nav-link">
+            <img src={errorGif} alt="Error Page" className="error-gif" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
