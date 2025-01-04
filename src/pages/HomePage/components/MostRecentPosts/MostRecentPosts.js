4fe79b9bc7ddd95d7ea0a7c3ea55240d9ff4f2ef
Roy Schor <rschor@justworks.com>
2025-01-04 02:26:05 -0500

    added recent blogs to home page

---
+++ src/pages/HomePage/components/MostRecentPosts/MostRecentPosts.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../../../../styles/layout.css";
+import "../../../BlogPage/BlogPage.css";
+import "./MostRecentPosts.css";
+import blogs from "../../../../data/blogs";
+
+const MostRecentPosts = () => {
+  const mostRecentBlogs = blogs
+    .sort((a, b) => b.created_at - a.created_at)
+    .slice(0, 4);
+
+  return (
+    <div className="recent-posts-container">
+      <div className="recent-posts-text-container">
+        The Latest from the Blog
+      </div>
+      <div className="blog-grid">
+        {mostRecentBlogs.map((blog) => {
+          const imagePath = require(
+            `../../../../assets/blog/${blog.folder}/${blog.background_image}.jpg`,
+          );
+          return (
+            <div className="blog-item-wrapper" key={blog.id}>
+              <div className="blog-item">
+                <a href={blog.path} className="blog-link">
+                  <div className="blog-image-wrapper">
+                    <div className="most-recent-blog-title">{blog.title}</div>
+
+                    <div
+                      className="blog-image"
+                      style={{ backgroundImage: `url(${imagePath})` }}
+                    >
+                      <div className="blog-description">
+                        {blog.blog_description}
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MostRecentPosts;
--- src/pages/HomePage/components/RotatingGallery/RotatingGallery.js
+++ src/pages/HomePage/components/RotatingGallery/RotatingGallery.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import "../../../styles/layout.css";
+import "../../../../styles/layout.css";
+import "./RotatingGallery.css";
 
 const RotatingGallery = () => {
   const importAll = (r) => r.keys().map(r);
+
   const images = importAll(
     require.context(
-      "../../../assets/homePageGallery",
+      "../../../../assets/homePageGallery",
       false,
       /\.(png|jpe?g|svg)$/,
     ),
@@ -45,37 +47,31 @@ const RotatingGallery = () => {
   );
 
   return (
-    <div className="relative bg-white py-10">
-      <div className="max-w-5xl mx-auto px-4 relative">
-        <div className="flex gap-4 justify-center">
+    <div className="gallery-container">
+      <div className="sliding-gallery-text-container">
+        <h2 className="sliding-gallery-title">
+          Discover. Wonder. Experience. Live.
+        </h2>
+      </div>
+      <div className="rotating-gallery-container">
+        <div className="rotating-gallery-image-container">
           {currentImages.map((image, index) => (
-            <div
-              key={index}
-              className="relative w-1/3 h-64 bg-gray-200 rounded-lg overflow-hidden shadow-lg"
-            >
+            <div key={index} className="rotating-gallery-image-box">
               <img
                 src={image.default || image}
                 alt={`Gallery ${currentIndex + index + 1}`}
-                className="w-full h-full object-cover"
+                className="rotating-gallery-image"
               />
             </div>
           ))}
         </div>
-
-        <button
-          onClick={prevSlide}
-          className="absolute top-1/3 left-4 transform translate-y-1/2 bg-gray-800 text-white w-10 h-10 rounded-md hover:bg-gray-700 focus:outline-none"
-        >
+        <button onClick={prevSlide} className="rotating-gallery-nav-button ">
           ❮
         </button>
-        <button
-          onClick={nextSlide}
-          className="absolute top-1/3 right-4 transform translate-y-1/2 bg-gray-800 text-white w-10 h-10 rounded-md hover:bg-gray-700 focus:outline-none"
-        >
+        <button onClick={nextSlide} className="rotating-gallery-nav-button">
           ❯
         </button>
-
-        <div className="text-center mt-4 text-lg text-gray-600">
+        <div className="rotating-gallery-image-counter">
           {currentIndex + 1} -{" "}
           {Math.min(currentIndex + imagesPerSlide, totalImages)} / {totalImages}
         </div>
