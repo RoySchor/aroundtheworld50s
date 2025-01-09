21ce591054da93a8f56e08d0732fbb59d03c5524
Roy Schor <rschor@justworks.com>
2025-01-09 01:05:27 -0500

    added set coordinates for countries

---
+++ src/pages/DestinationsPage/components/WorldMap.constants.js
@@ -0,0 +1,58 @@
+export const countryCoordinates = {
+  Belize: {
+    left: "28%",
+    top: "55.2%",
+  },
+  Bermuda: {
+    left: "34%",
+    top: "47.4%",
+  },
+  Denmark: {
+    left: "50.8%",
+    top: "36.5%",
+  },
+  England: {
+    left: "48.3%",
+    top: "38.4%",
+  },
+  France: {
+    left: "48.6%",
+    top: "40.9%",
+  },
+  Israel: {
+    left: "57.5%",
+    top: "47.4%",
+  },
+  Italy: {
+    left: "52%",
+    top: "43.5%",
+  },
+  Jamaica: {
+    left: "29.5%",
+    top: "53.3%",
+  },
+  Norway: {
+    left: "50.4%",
+    top: "34%",
+  },
+  "Puerto Rico": {
+    left: "32.5%",
+    top: "53.3%",
+  },
+  Russia: {
+    left: "57%",
+    top: "37.4%",
+  },
+  Spain: {
+    left: "48.3%",
+    top: "44.3%",
+  },
+  Sweden: {
+    left: "52%",
+    top: "32.8%",
+  },
+  "Trinidad and Tobago": {
+    left: "32.9%",
+    top: "56.3%",
+  },
+};
--- src/pages/DestinationsPage/components/WorldMap.js
+++ src/pages/DestinationsPage/components/WorldMap.js
@@ -2,37 +2,42 @@ import React, { useState } from "react";
 import "./WorldMap.css";
 import worldMap from "../../../assets/flat-world-map.webp";
 import blogs from "../../../data/blogs";
+import { countryCoordinates } from "./WorldMap.constants";
+import { serializeCountry } from "../DestinationPage.utils";
 
 const WorldMap = () => {
   const [hoveredCountry, setHoveredCountry] = useState(null);
 
-  const countryCoordinates = {
-    "Trinidad and Tobago": {
-      left: "32.9%",
-      top: "56.3%",
-    },
-  };
+  const uniqueCountryBlogs = Object.values(
+    blogs.reduce((acc, blog) => {
+      if (!acc[blog.country]) {
+        acc[blog.country] = blog;
+      }
+      return acc;
+    }, {}),
+  );
 
   return (
     <div className="world-map-container">
       <img src={worldMap} alt="World Map" className="world-map" />
 
-      {blogs.map((blog) => {
+      {uniqueCountryBlogs.map((blog) => {
         const coordinates = countryCoordinates[blog.country];
         if (!coordinates) return null;
 
         return (
-          <div
-            key={blog.id}
-            className="country-box-selector"
-            style={{
-              left: coordinates.left,
-              top: coordinates.top,
-              transform: "translate(-50%, -50%)",
-            }}
-            onMouseEnter={() => setHoveredCountry(blog)}
-            onMouseLeave={() => setHoveredCountry(null)}
-          />
+          <a href={`/blog/${serializeCountry(blog.country)}`} key={blog.id}>
+            <div
+              className="country-box-selector"
+              style={{
+                left: coordinates.left,
+                top: coordinates.top,
+                transform: "translate(-50%, -50%)",
+              }}
+              onMouseEnter={() => setHoveredCountry(blog)}
+              onMouseLeave={() => setHoveredCountry(null)}
+            />
+          </a>
         );
       })}
 
