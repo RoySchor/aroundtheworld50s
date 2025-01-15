2dd6d9ee14b6745e8d6792b2f43430afaaad4737
Roy Schor <rschor@justworks.com>
2025-01-15 00:15:17 -0500

    changes to HomePage and AboutMe Page, minor change to Trinidad blog

--- src/pages/AboutMePage/AboutMePage.js
+++ src/pages/AboutMePage/AboutMePage.js
@@ -5,40 +5,55 @@ import aboutMeImage from "../../assets/about-me-page-bg.webp";
 const AboutMePage = () => {
   return (
     <div className="page-container about-me">
-      <div className="section-container">
-        <div className="top-section-container">
-          <img src={aboutMeImage} alt="About Me" className="image-container" />
+      <div className="about-me-section-container">
+        <div className="about-me-top-section-container">
+          <img
+            src={aboutMeImage}
+            alt="About Me"
+            className="about-me-image-container"
+          />
 
           <div className="overlap-text-container-box">
             <h1 className="overlap-text-container-box-title ">
-              Travel with us!
+              A Journey of Growth
             </h1>
             <p className="overlap-text-container-box-description">
-              As a couple in our 50s, we've embarked on countless adventures as
-              a duo and sometimes with our grown sons. We've had the privilege
-              of exploring many destinations. We're passionate about sharing our
-              experiences through detailed itineraries, honest tips and reviews,
-              along with breathtaking photography. Whether you're a seasoned
-              traveler or a first-time explorer, we're here to inspire and guide
-              you on your next adventure.
+              Our love for travel has evolved over the years, long before the
+              age of smartphones. We remember those early days of planning trips
+              with paper maps and Mapquest, meticulously plotting our routes.
+              From those humble beginnings, we've embraced the evolution of
+              travel planning, incorporating GPS, travel apps, and even AI.
             </p>
           </div>
         </div>
       </div>
 
-      <div className="section-container">
-        <div className="bottom-section-container">
-          <h2 className="bottom-section-container-title">All About Me</h2>
-          <p className="bottom-section-container-description">
-            Join us as we uncover hidden gems, immerse ourselves in diverse
-            cultures, and savor the world's beauty. Planning a trip can be
-            overwhelming. Scrolling through countless reviews and videos can
-            leave you feeling lost and trying to figure out where to start.
+      <div className="about-me-section-container">
+        <div className="about-me-bottom-section-container">
+          <h2 className="about-me-bottom-section-container-title">
+            What We're All About
+          </h2>
+          <p className="about-me-bottom-section-container-description">
+            From family road trips with our children to spontaneous city
+            escapes. Even when not traveling, we constantly explore and discover
+            hidden gems in our NYC neighborhoods. We explore hidden corners
+            every weekend, seeking unique experiences and off-the-beaten-path
+            adventures. Believe me, there's always something new to discover in
+            this vibrant city! You'll find us sharing our travel adventures
+            alongside unique city experiences – the unexpected encounters, the
+            hidden cafes, and the off-the-beaten-path discoveries that make
+            travel truly special.
           </p>
-          <p className="bottom-section-container-description">
-            Let us leverage our experience to help you plan a seamless and
-            unforgettable trip. You can minimize stress and maximize enjoyment
-            with our insider tips and expert advice.
+          <p className="about-me-bottom-section-container-description">
+            We're passionate about sharing our travel experiences with you, from
+            epic family adventures like dog sledding in the Canadian wilderness
+            and living on a houseboat for a couple of weeks to the simple joy of
+            discovering a charming cafe on a quiet Manhattan street. Join us as
+            we explore the world, one adventure at a time. I love to take
+            photos, and I'll be sharing my photos and videos alongside our
+            travel stories and city explorations right here on the blog and on
+            my Instagram and TikTok. Follow along as we capture the essence of
+            each journey through our lens.
           </p>
         </div>
       </div>
