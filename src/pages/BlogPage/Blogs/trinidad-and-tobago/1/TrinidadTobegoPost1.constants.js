8c034ad32634bde628339d7d6d2e1f777b29abc6
Roy Schor <rschor@justworks.com>
2025-04-07 01:42:29 -0400

    removing text into constants file

---
+++ src/pages/BlogPage/Blogs/trinidad-and-tobago/1/TrinidadTobegoPost1.constants.js
@@ -0,0 +1,153 @@
+export const TRINIDAD_TOBAGO_POST_1 = {
+  header: "📍🇹🇹 Trinidad & Tobago: A Tale of Two Islands",
+  title: "📌Port-of-Spain",
+  subtitle: "⛱️10 days of Caribbean Charm: Uncovering the Soul and Beaches ✨",
+  description: `
+    Our Trinidad & Tobago adventure was a spur-of-the-moment decision.
+    When our cruise was unexpectedly rerouted (due to a hurricane), we embraced
+    the unexpected and planned a last-minute island getaway.
+    The idea of exploring these vibrant twin islands, fueled by my husband's
+    friend's glowing descriptions of the culture and beaches, quickly took hold.
+    And so, our Caribbean escape began.
+  `,
+  tipsSection:
+    "💥 Insider Tips: Your Key to an Unforgettable Trip (read more…)",
+
+  itineraryOne: {
+    title: "Itinerary 📍",
+    items: [
+      "📌 The BRIX, Autograph Collection",
+      "📌 Queen's Hall",
+      "📌 Stollmeyer's Castle Killarney",
+      "📌 Memorial Park",
+      "📌 Queen's Park Savannah",
+    ],
+  },
+
+  itineraryTwo: {
+    title: "Itinerary 📍",
+    items: [
+      "📌 Ariapita Avenue",
+      "📌 Independence Square",
+      "📌 Memorial Park",
+      "📌 Downtown Port of Spain",
+      "📌 Trinity Cathedral",
+      "📌 Cathedral of the Immaculate Conception",
+      "📌 Independence Square",
+    ],
+  },
+
+  sections: {
+    arrivalDescription: `
+      The 🛩️ touched down in 📌Port of Spain just before noon. We hailed a 🚕taxi
+      to our hotel, The #BRIX, Autograph Collection. We selected this 🏨for its
+      upscale accommodations, great deal, and proximity to key attractions like
+      📌Queen's Park.
+    `,
+
+    magnificentSeven: `
+      We strolled down the street, passing the historic 📌Queen's Hall, heading
+      towards the 📌Magnificent Seven, a collection of seven stunning Victorian-era
+      mansions on Maraval Road in #PortofSpain. These 🏰architectural gems, built
+      between 1902 and 1910, offer a glimpse into the island's opulent past.
+      We strolled along the street, admiring their unique architectural styles,
+      including French Colonial, Scottish baronial, and Indian Empire influences.
+      The intricate details of each mansion while imagining the lives of the
+      families who once resided within their walls.
+    `,
+
+    memorialPark: `
+      As we strolled leisurely through 📌Memorial Park and the iconic 📌Queen's
+      Park Oval, we couldn't resist indulging in this cold, refreshing 🥥coconut
+      water from street vendors.
+    `,
+
+    downtownDescription: `
+      As it was still early afternoon, we hopped into a 🚕, heading downtown to
+      the vibrant soul, a blend of history and culture. Downtown Port of Spain
+      is a whirlwind of activity, where the city's rhythm pulses through the
+      streets. Historic landmarks stand proudly alongside modern buildings while
+      the air is filled with the sounds of lively chatter, soca music, and the
+      tempting aromas of street food. It's a captivating blend of history and
+      modernity with grand colonial buildings, bustling markets, and tranquil squares.
+    `,
+
+    independenceSquare: `
+      We asked the driver to drop us off few blocks from 📌Independence Square,
+      so we could soak in the vibrant #atmosphere of downtown. Independence Square
+      was a bustling hub of activity, with people going about their day and street
+      vendors selling their wares. We wandered through the square, soaking up the
+      vibrant atmosphere and admiring the architecture of the surrounding buildings.
+      Towering government buildings, like silent sentinels, watched over the square.
+      We passed the Red House, the seat of Parliament, a grand building that
+      whispered tales of political history. The Supreme Court, a majestic edifice,
+      stood nearby, a reminder of the island's legal system. It was a fascinating
+      walk, a journey through the heart of Trinidad and Tobago's governance.
+    `,
+
+    cathedral: `
+      The square pulsed with the vibrant energy of this Caribbean capital
+      surrounded by historic government buildings. We approached the majestic
+      📌Cathedral of the Immaculate Conception⛪, which loomed against the lively
+      Port of Spain skyline. As we approached, its imposing presence commanded
+      attention by its intricate stonework, the soaring spires, and the sense
+      of tranquility. Stepping inside was like entering another world of
+      awe-inspiring beauty. Sunlight streamed through stained glass windows,
+      casting colors across the polished marble floor.
+    `,
+
+    market: `
+      We were drawn into the lively #market, a sensory explosion! We navigated
+      the bustling crowds, captivated by the vibrant display of fruits and
+      vegetables. Mountains of pineapples, peppers, and many other colorful
+      produce overflowed from every stall, creating an exhilarating experience
+      that immersed us in the local culture.
+    `,
+
+    trinityCathedral: `
+      We continued our exploration, venturing towards 📌Trinity Cathedral⛪,
+      architectural beauty, a breathtaking testament to the island's history.
+      Stepping inside was like stepping back in time. We continued along the
+      road, passing charming gardens adorned with sculptures, until we reached
+      the traffic light where the police station was located, and our #journey
+      came to an unexpected halt. A friendly 👮police officer advised us against
+      proceeding further, citing safety concerns for tourists in that particular area.
+    `,
+
+    ariapitaAvenue: `
+      We retraced our steps back towards the vibrant market and hailed another
+      🚕. Our destination? 📌#Ariapita Avenue, renowned as Port of Spain's "foodie
+      street". We envisioned a lively culinary scene with many restaurants and
+      some street vendors. Unfortunately, our expectations weren't fully met.
+      The street seemed a bit subdued, with several establishments closed. We
+      weren't sure if the time of day (around 7 PM) or perhaps the off-season
+      (October) contributed to the quieter atmosphere.
+    `,
+
+    memorialParkDinner: `
+      Hungry, we followed the hotel's recommendation and returned to Memorial
+      Park, eager to sample the local flavors. We paused at the Cenotaph Garden,
+      paying our respects at this poignant memorial within Memorial Park. We were
+      excited to discover a vibrant food truck scene, a delicious and lively way
+      to experience the local cuisine at the southern point of the park across
+      from the performance art center. It was the perfect place to experience
+      authentic local cuisine.
+    `,
+
+    evening: `
+      Back at the hotel, we retreated to the rooftop 🍻bar of our 🏨hotel.
+      Sipping on cool drinks🍹, we relaxed and enjoyed breathtaking panoramic
+      views of the city sparkling below. A delightful surprise awaited us in
+      our room! A beautifully arranged tray laden with decadent cakes sat on
+      the table, a thoughtful gesture from the hotel to celebrate my husband's
+      birthday and our anniversary. What a wonderful way to end the day!
+    `,
+
+    surprise: `
+      A delightful surprise awaited us in our room! A beautifully arranged
+      tray laden with decadent cakes sat on the table, a thoughtful gesture
+      from the hotel to celebrate my husband's birthday and our anniversary.
+      What a wonderful way to end the day!
+    `,
+  },
+};
--- src/pages/BlogPage/Blogs/trinidad-and-tobago/1/TrinidadTobegoPost1.js
+++ src/pages/BlogPage/Blogs/trinidad-and-tobago/1/TrinidadTobegoPost1.js
@@ -5,6 +5,7 @@ import background from "../../../../../assets/blog/trinidad-and-tobago/1/trinida
 import TwoColumnLayout from "../../../../../components/TwoColumnLayout/TwoColumnLayout";
 import ImageGrid from "../../../../../components/ImageGrid/ImageGrid";
 import MapEmbed from "../../../../../components/MapEmbed/MapEmbed";
+import { TRINIDAD_TOBAGO_POST_1 } from "./TrinidadTobegoPost1.constants";
 
 const TrinidadTobegoPost1 = () => {
   const images = [
@@ -44,52 +45,31 @@ const TrinidadTobegoPost1 = () => {
       >
         <div className="fixed-background-text-container">
           <div className="fixed-background-title fixed-background-no-margin">
-            📍🇹🇹 Trinidad & Tobago: A Tale of Two Islands
+            {TRINIDAD_TOBAGO_POST_1.header}
           </div>
         </div>
       </div>
 
       <div className="container">
         <div className="page-content">
-          <div className="post-title">📌Port-of-Spain</div>
+          <div className="post-title">{TRINIDAD_TOBAGO_POST_1.title}</div>
 
-          <div className="post-subtitle">
-            ⛱️10 days of Caribbean Charm: Uncovering the Soul and Beaches ✨
-          </div>
+          <div className="post-subtitle">{TRINIDAD_TOBAGO_POST_1.subtitle}</div>
 
           <div className="post-description">
-            Our Trinidad & Tobago adventure was a spur-of-the-moment decision.
-            When our cruise was unexpectedly rerouted (due to a hurricane), we
-            embraced the unexpected and planned a last-minute island getaway.
-            The idea of exploring these vibrant twin islands, fueled by my
-            husband's friend's glowing descriptions of the culture and beaches,
-            quickly took hold. And so, our Caribbean escape began.
+            {TRINIDAD_TOBAGO_POST_1.description}
           </div>
 
-          {/* TODO: Wrap this in Link to Tips Page */}
           <div className="post-bolded-text post-tips-section-container">
-            💥 Insider Tips: Your Key to an Unforgettable Trip (read more…)
+            {TRINIDAD_TOBAGO_POST_1.tipsSection}
           </div>
 
           <TwoColumnLayout
             leftPane={{
               type: "list",
-              listTitle: "Itinerary 📍",
-              listItems: [
-                "📌 The BRIX, Autograph Collection",
-                "📌 Queen's Hall",
-                "📌 Stollmeyer's Castle Killarney",
-                "📌 Memorial Park",
-                "📌 Queen's Park Savannah",
-              ],
+              listTitle: TRINIDAD_TOBAGO_POST_1.itineraryOne.title,
+              listItems: TRINIDAD_TOBAGO_POST_1.itineraryOne.items,
             }}
-            // To Retreive the Embeded map url you need:
-            // 1. Go to the Google Maps created map
-            // 2. Click on the hamburger / menu button on the top left
-            // 3. Click on the "Share or embed map button
-            // 4. Go to the "Embed a map" tab
-            // 5. Copy just the link from the provided html code.
-            // 5. Paste it in the url field
             rightPane={{
               type: "map",
               mapComponent: portOfSpainMap,
@@ -106,21 +86,12 @@ const TrinidadTobegoPost1 = () => {
             }}
             rightPane={{
               type: "text",
-              content:
-                "The 🛩️ touched down in 📌Port of Spain just before noon. We hailed a 🚕taxi to our hotel, The #BRIX, Autograph Collection. We selected this 🏨for its upscale accommodations, great deal, and proximity to key attractions like 📌Queen's Park.",
+              content: TRINIDAD_TOBAGO_POST_1.sections.arrivalDescription,
             }}
           />
 
           <div className="post-description">
-            We strolled down the street, passing the historic 📌Queen's Hall,
-            heading towards the 📌Magnificent Seven, a collection of seven
-            stunning Victorian-era mansions on Maraval Road in #PortofSpain.
-            These 🏰architectural gems, built between 1902 and 1910, offer a
-            glimpse into the island's opulent past. We strolled along the
-            street, admiring their unique architectural styles, including French
-            Colonial, Scottish baronial, and Indian Empire influences. The
-            intricate details of each mansion while imagining the lives of the
-            families who once resided within their walls.
+            {TRINIDAD_TOBAGO_POST_1.sections.magnificentSeven}
           </div>
 
           <ImageGrid images={images} />
@@ -133,35 +104,19 @@ const TrinidadTobegoPost1 = () => {
             }}
             rightPane={{
               type: "text",
-              content:
-                "As we strolled leisurely through 📌Memorial Park and the iconic 📌Queen's Park Oval, we couldn't resist indulging in this cold, refreshing 🥥coconut water from street vendors.",
+              content: TRINIDAD_TOBAGO_POST_1.sections.memorialPark,
             }}
           />
 
           <div className="post-description">
-            As it was still early afternoon, we hopped into a 🚕, heading
-            downtown to the vibrant soul, a blend of history and culture.
-            Downtown Port of Spain is a whirlwind of activity, where the city's
-            rhythm pulses through the streets. Historic landmarks stand proudly
-            alongside modern buildings while the air is filled with the sounds
-            of lively chatter, soca music, and the tempting aromas of street
-            food. It's a captivating blend of history and modernity with grand
-            colonial buildings, bustling markets, and tranquil squares.
+            {TRINIDAD_TOBAGO_POST_1.sections.downtownDescription}
           </div>
 
           <TwoColumnLayout
             leftPane={{
               type: "list",
-              listTitle: "Itinerary 📍",
-              listItems: [
-                "📌 Ariapita Avenue",
-                "📌 Independence Square",
-                "📌 Memorial Park",
-                "📌 Downtown Port of Spain",
-                "📌 Trinity Cathedral",
-                "📌 Cathedral of the Immaculate Conception",
-                "📌 Independence Square",
-              ],
+              listTitle: TRINIDAD_TOBAGO_POST_1.itineraryTwo.title,
+              listItems: TRINIDAD_TOBAGO_POST_1.itineraryTwo.items,
             }}
             rightPane={{
               type: "map",
@@ -177,81 +132,38 @@ const TrinidadTobegoPost1 = () => {
             }}
             rightPane={{
               type: "text",
-              content:
-                "We asked the driver to drop us off few blocks from 📌Independence Square, so we could soak in the vibrant #atmosphere of downtown. Independence Square was a bustling hub of activity, with people going about their day and street vendors selling their wares. We wandered through the square, soaking up the vibrant atmosphere and admiring the architecture of the surrounding buildings. Towering government buildings, like silent sentinels, watched over the square. We passed the Red House, the seat of Parliament, a grand building that whispered tales of political history. The Supreme Court, a majestic edifice, stood nearby, a reminder of the island's legal system. It was a fascinating walk, a journey through the heart of Trinidad and Tobago's governance.",
+              content: TRINIDAD_TOBAGO_POST_1.sections.independenceSquare,
             }}
           />
 
           <ImageGrid images={images} />
 
           <div className="post-description">
-            The square pulsed with the vibrant energy of this Caribbean capital
-            surrounded by historic government buildings. We approached the
-            majestic 📌Cathedral of the Immaculate Conception⛪, which loomed
-            against the lively Port of Spain skyline. As we approached, its
-            imposing presence commanded attention by its intricate stonework,
-            the soaring spires, and the sense of tranquility. Stepping inside
-            was like entering another world of awe-inspiring beauty. Sunlight
-            streamed through stained glass windows, casting colors across the
-            polished marble floor.
+            {TRINIDAD_TOBAGO_POST_1.sections.cathedral}
           </div>
 
           <div className="post-description">
-            We were drawn into the lively #market, a sensory explosion! We
-            navigated the bustling crowds, captivated by the vibrant display of
-            fruits and vegetables. Mountains of pineapples, peppers, and many
-            other colorful produce overflowed from every stall, creating an
-            exhilarating experience that immersed us in the local culture.
+            {TRINIDAD_TOBAGO_POST_1.sections.market}
           </div>
 
           <div className="post-description">
-            We continued our exploration, venturing towards 📌Trinity
-            Cathedral⛪, architectural beauty, a breathtaking testament to the
-            island's history. Stepping inside was like stepping back in time. We
-            continued along the road, passing charming gardens adorned with
-            sculptures, until we reached the traffic light where the police
-            station was located, and our #journey came to an unexpected halt. A
-            friendly 👮police officer advised us against proceeding further,
-            citing safety concerns for tourists in that particular area.
+            {TRINIDAD_TOBAGO_POST_1.sections.trinityCathedral}
           </div>
 
           <div className="post-description">
-            We retraced our steps back towards the vibrant market and hailed
-            another 🚕. Our destination? 📌#Ariapita Avenue, renowned as Port of
-            Spain's "foodie street". We envisioned a lively culinary scene with
-            many restaurants and some street vendors. Unfortunately, our
-            expectations weren't fully met. The street seemed a bit subdued,
-            with several establishments closed. We weren't sure if the time of
-            day (around 7 PM) or perhaps the off-season (October) contributed to
-            the quieter atmosphere.
+            {TRINIDAD_TOBAGO_POST_1.sections.ariapitaAvenue}
           </div>
 
           <div className="post-description">
-            Hungry, we followed the hotel's recommendation and returned to
-            Memorial Park, eager to sample the local flavors. We paused at the
-            Cenotaph Garden, paying our respects at this poignant memorial
-            within Memorial Park. We were excited to discover a vibrant food
-            truck scene, a delicious and lively way to experience the local
-            cuisine at the southern point of the park across from the
-            performance art center. It was the perfect place to experience
-            authentic local cuisine.
+            {TRINIDAD_TOBAGO_POST_1.sections.memorialParkDinner}
           </div>
 
           <div className="post-description">
-            Back at the hotel, we retreated to the rooftop 🍻bar of our 🏨hotel.
-            Sipping on cool drinks🍹, we relaxed and enjoyed breathtaking
-            panoramic views of the city sparkling below. A delightful surprise
-            awaited us in our room! A beautifully arranged tray laden with
-            decadent cakes sat on the table, a thoughtful gesture from the hotel
-            to celebrate my husband's birthday and our anniversary. What a
-            wonderful way to end the day!
+            {TRINIDAD_TOBAGO_POST_1.sections.evening}
           </div>
 
           <div className="post-description">
-            A delightful surprise awaited us in our room! A beautifully arranged
-            tray laden with decadent cakes sat on the table, a thoughtful
-            gesture from the hotel to celebrate my husband's birthday and our
-            anniversary. What a wonderful way to end the day!
+            {TRINIDAD_TOBAGO_POST_1.sections.surprise}
           </div>
         </div>
       </div>
