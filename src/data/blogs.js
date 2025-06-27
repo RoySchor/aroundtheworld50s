5ce4dd2edd23c40d3fbf804e2ac4d4c38a1eee30
Roy Schor <rschor@justworks.com>
2025-06-27 00:50:38 -0400

    removing blogs

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -10,29 +10,7 @@ const blogs = [
     path: "/blog/trinidad-and-tobago/1",
     blog_description:
       "⛱️10 days of Caribbean Charm: Uncovering the Soul and beaches✨",
-  },
-  {
-    id: "france-1",
-    created_at: "2025-06-27",
-    country: "France",
-    country_code: "FR",
-    title: "France Title",
-    folder: "france/1",
-    background_image: "frodo.jpg",
-    path: "/blog/france/1",
-    blog_description: "Descritpion for thumbnail",
-  },
-  {
-    id: "france-2",
-    created_at: "2025-06-27",
-    country: "France",
-    country_code: "FR",
-    title: "France Title",
-    folder: "france/2",
-    background_image: "frodo.jpg",
-    path: "/blog/france/2",
-    blog_description: "Descritpion for thumbnail",
-  },
+  }
 ];
 
 export default blogs;
