38683bcf34de6aff9ebc42858313b0856628ba07
Roy Schor <rschor@justworks.com>
2025-07-18 22:19:16 -0400

    adding blog sorting to the blog page as well

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -5,13 +5,33 @@ import { Link } from "react-router-dom";
 import blogs from "../../data/blogs";
 
 const BlogPage = () => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date
+      .toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+      })
+      .replace(/\d+/, (day) => {
+        const suffixes = { 1: "st", 2: "nd", 3: "rd" };
+        const suffix = suffixes[day] || "th";
+        return `${day}${suffix}`;
+      });
+  };
+
+  const sortedBlogs = [...blogs].sort((a, b) => {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    return dateB - dateA; // Sort in descending order (newest first)
+  });
+
   return (
     <div className="page-container blog">
       <div className="container">
         <div className="page-content">
           <h1 className="page-title">Blog</h1>
           <div className="blog-grid">
-            {blogs.map((blog) => {
+            {sortedBlogs.map((blog) => {
               const imagePath = require(
                 `../../assets/blog/${blog.folder}/${blog.background_image}`,
               );
@@ -21,7 +41,6 @@ const BlogPage = () => {
                     <Link to={blog.path} className="blog-link">
                       <div className="blog-image-wrapper">
                         <div className="blog-title">{blog.title}</div>
-
                         <div
                           className="blog-image"
                           style={{ backgroundImage: `url(${imagePath})` }}
@@ -30,6 +49,9 @@ const BlogPage = () => {
                             {blog.blog_description}
                           </div>
                         </div>
+                      </div>
+                      <div className="blog-date">
+                        {formatDate(blog.created_at)}
                       </div>
                     </Link>
                   </div>
