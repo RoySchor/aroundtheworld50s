564b972e84cdab9f5658c641a24d061103b6d3dd
Roy Schor <rschor@justworks.com>
2025-07-21 18:10:43 -0400

    fixing some additional issues

--- src/components/ContentPane/ContentPane.js
+++ src/components/ContentPane/ContentPane.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../../pages/BlogPage/Blogs/BlogPost.css";
+import "./ContentPane.css";
 
 const ContentPane = ({
   type,
@@ -15,38 +16,46 @@ const ContentPane = ({
     switch (type) {
       case "text":
         return (
-          <div
-            className="post-description"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
+          <div className="content-pane-text">
+            <div
+              className="post-description"
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
+          </div>
         );
       case "image":
         return (
-          <div className="image-container">
-            <img src={imageUrl} alt={imageAlt || "Content image"} />
+          <div className="content-pane-image">
+            <div className="image-container">
+              <img src={imageUrl} alt={imageAlt || "Content image"} />
+            </div>
           </div>
         );
       case "list":
         return (
-          <div className="post-itinerary-section">
-            <div className="post-itinerary-title">{listTitle}</div>
-            <ul>
-              {listItems.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
+          <div className="content-pane-list">
+            <div className="post-itinerary-section">
+              <div className="post-itinerary-title">{listTitle}</div>
+              <ul>
+                {listItems.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
           </div>
         );
       case "map":
-        return mapComponent;
+        return <div className="content-pane-map">{mapComponent}</div>;
       case "custom":
-        return content;
+        return <div className="content-pane-custom">{content}</div>;
       default:
         return null;
     }
   };
 
-  return renderContent();
+  return (
+    <div className={`content-pane content-pane-${type}`}>{renderContent()}</div>
+  );
 };
 
 ContentPane.propTypes = {
