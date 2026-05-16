7d4d8b9fd870c41e661b34cf3fea333ea17d9710
Roy Schor <rschor@justworks.com>
2026-05-15 21:27:38 -0400

    chore: remove dead v1 scripts, replace boilerplate README

--- start_dev_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Change to the script's directory (which is the project root)
-cd "$SCRIPT_DIR"
-
-# Start the development server
-npm start
